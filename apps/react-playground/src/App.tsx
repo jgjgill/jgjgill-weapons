ac9487fe6298d8f572858cef10c2838e0e2b73d8
jgjgill <dbdltm22@naver.com>
2024-09-21 19:36:50 +0900

    feat: add react-playground

---
+++ apps/react-playground/src/App.tsx
@@ -0,0 +1,12 @@
+import { add } from "@jgjgill/math";
+
+function App() {
+  return (
+    <div>
+      <h2>hello world jgjgill</h2>
+      {add(1, 4)}
+    </div>
+  );
+}
+
+export default App;
--- apps/web/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { add } from "@jgjgill/math";
-
-export default function Home() {
-  return <div>{add(1, 2)}</div>;
-}
