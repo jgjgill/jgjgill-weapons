7aa3a123c1fbfbb798c4d7c6225dd878898bb467
jgjgill <dbdltm22@naver.com>
2024-09-22 20:18:36 +0900

    feat: add biome ignore files

--- apps/react-playground/src/App.tsx
+++ apps/react-playground/src/App.tsx
@@ -1,19 +1,12 @@
 import { useUnmount } from "@jgjgill/hooks";
-import { useEffect } from "react";
 
 function App() {
 	useUnmount(() => {
 		console.log("unmount");
 	});
 
-	const a = 123;
-	useEffect(() => {});
-
 	return (
 		<div>
-			{[1, 2, 3, 4].map((item) => (
-				<div>123</div>
-			))}
 			<h2>hello world jgjgill</h2>
 		</div>
 	);
