b932d66033568eda91ec54a6be092451f61c712c
jgjgill <dbdltm22@naver.com>
2024-09-22 20:03:02 +0900

    feat: remove eslint

--- apps/react-playground/src/App.tsx
+++ apps/react-playground/src/App.tsx
@@ -1,14 +1,22 @@
 import { useUnmount } from "@jgjgill/hooks";
+import { useEffect } from "react";
 
 function App() {
-  useUnmount(() => {
-    console.log("unmount");
-  });
-  return (
-    <div>
-      <h2>hello world jgjgill</h2>
-    </div>
-  );
+	useUnmount(() => {
+		console.log("unmount");
+	});
+
+	const a = 123;
+	useEffect(() => {});
+
+	return (
+		<div>
+			{[1, 2, 3, 4].map((item) => (
+				<div>123</div>
+			))}
+			<h2>hello world jgjgill</h2>
+		</div>
+	);
 }
 
 export default App;
