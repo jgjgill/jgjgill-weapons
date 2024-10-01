188d47585728092caea049c70fec5964cafe47d9
jgjgill <dbdltm22@naver.com>
2024-10-01 20:55:55 +0900

    chore: test hooks

--- apps/react-playground/src/App.tsx
+++ apps/react-playground/src/App.tsx
@@ -1,13 +1,24 @@
-import { useUnmount } from "@jgjgill/hooks";
+import { useIsChanged, usePrevious, useUnmount } from "@jgjgill/hooks";
+import { useState } from "react";
 
 function App() {
+	const [count, setCount] = useState(0);
+
 	useUnmount(() => {
 		console.log("unmount");
 	});
 
+	const prevCount = usePrevious(count);
+	const isChanged = useIsChanged(count);
+
 	return (
 		<div>
-			<h2>hello world jgjgill</h2>
+			<p>Current Count: {count}</p>
+			<p>Previous Count: {prevCount}</p>
+			<p>Has Count Changed?: {isChanged ? "Yes" : "No"}</p>
+			<button type="button" onClick={() => setCount(count + 1)}>
+				Increment
+			</button>
 		</div>
 	);
 }
