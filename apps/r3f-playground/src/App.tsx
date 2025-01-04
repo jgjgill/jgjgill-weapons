c7ca038b400e49d65f0316c34effb251b2bdfd3b
jgjgill <dbdltm22@naver.com>
2025-01-04 22:46:31 +0900

    chore: r3f test

--- apps/r3f-playground/src/App.tsx
+++ apps/r3f-playground/src/App.tsx
@@ -1,33 +1,39 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
-import { Box, Dice } from "@jgjgill/interaction";
+import { Box, Dice, Nameplate } from "@jgjgill/interaction";
 import { OrbitControls } from "@react-three/drei";
 import MyElement3D from "./MyElement3D";
+import qwd from "/mapcap.jpg";
 
 function App() {
 	return (
-		<Canvas
-			shadows
-			style={{
-				background: "black",
-			}}
-			camera={{ position: [2, 2, 2] }}
-		>
-			<Dice
-				position={[-2, 0, 0]}
-				onFinish={(value) => {
-					console.log(value);
-				}}
-			/>
-			<Box position={[2, 0, 0]} />
-			<OrbitControls autoRotate />
+		<>
+			<Nameplate src="/mapcap.jpg" mainText="qwe" subText="aa" />
+			<div>
+				<Canvas
+					shadows
+					style={{
+						background: "black",
+					}}
+					camera={{ position: [2, 2, 2] }}
+				>
+					<Dice
+						position={[-2, 0, 0]}
+						onFinish={(value) => {
+							console.log(value);
+						}}
+					/>
+					<Box position={[2, 0, 0]} />
+					<OrbitControls autoRotate />
 
-			<ambientLight intensity={0.5} />
-			<pointLight position={[1, 1, 1]} />
+					<ambientLight intensity={0.5} />
+					<pointLight position={[1, 1, 1]} />
 
-			{/* <MyElement3D /> */}
-		</Canvas>
+					{/* <MyElement3D /> */}
+				</Canvas>
+			</div>
+		</>
 	);
 }
 
